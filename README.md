c3758b301a10eb715419815333775147f8a39818
Avish Wagde <avishwagde13@gmail.com>
2025-08-02 18:42:43 +0900

    3- data ingestion pipeline working

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # MLOps_project
 This project implements a end to end machine learning project with industry standard MLOps pratices.
+
+
+## Workflows
+
+1. Update config.yaml
+2. Update schema.yaml
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline 
+8. Update the main.py
+9. Update the app.py
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from mlproject import logger
+from mlproject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("this is logging testuindjsvbfshjvbsdjvng")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<")
+except Exception as e:
+    logger.exception(e)
