b2a1b7fe626596077d204b9a6f2cee1ca0c04e2b
Avish Wagde <avishwagde13@gmail.com>
2025-08-03 23:02:54 +0900

    7 - model evaluation implemented, mlflow partially working

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from mlproject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipel
 from mlproject.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 from mlproject.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
 from mlproject.pipeline.stage04_model_training import ModelTrainingPipeline
+from mlproject.pipeline.stage05_model_evaluation import ModelEvaluationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -45,6 +46,17 @@ STAGE_NAME = "model training stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
     obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<")
+except Exception as e:
+    logger.exception(e)
+
+
+STAGE_NAME = "model evaluation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = ModelEvaluationPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<")
 except Exception as e:
